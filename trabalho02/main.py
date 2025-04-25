9772d4addbcb7ae92293e3d74b5053f16a5d2d58
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2025-04-25 10:19:47 -0300

    Implementei o Método de Newton-Rapshon

--- trabalho02/main.py
+++ trabalho02/main.py
@@ -27,6 +27,8 @@ def bissecao(f, a, b):
     fa = f(a)
     fb = f(b)
 
+    print("\n=========== Método da Bisseção ===========")
+
     # Verificando se há uma raiz no intervalo [a, b] fornecido:
     if fa * fb > 0:
         print(f"Não existe raiz no intervalo [{a}, {b}].")
@@ -39,13 +41,11 @@ def bissecao(f, a, b):
             fx = f(x)
 
             # Imprindo a iteração atual:
-            print(f"Iteração {i}:")
+            print(f"\nIteração {i}:")
             print(f"\tIntervalo [{a}, {b}]\n\tx = {x}\n\tf(x) = {fx}")
 
-            # Verifica a condição de parada:
-            if abs(fx) < E2 or ((b-a)/2) < E1:
-                print("\nResultado final:")
-                print(f"\tNúmero de iterações: {i}\n\tx = {x}\n\tf(x) = {fx}")
+            # Verifica as condições de parada:
+            if ((b-a)/2) < E1 or abs(fx) < E2:
                 break
             
             # A raiz está no novo intervalo [a, x]:
@@ -63,7 +63,33 @@ def bissecao(f, a, b):
 
 # Método de Newton-Raphson:
 def newton_raphson(f, derivada, x0):
-    pass
+    x = x0
+
+    print("\n=========== Método de Newton-Rapshon ===========")
+
+    i = 1
+    # Enquanto o número máximo de iterações não for atingido:
+    while i <= MAX_ITERACOES:
+        fx = f(x)
+        f_linha = derivada(x) # obtém a derivada da função no ponto x
+        
+        # Se a derivada da função em um ponto for 0, o algoritmo se encerra:
+        try:
+            delta_x = -fx/f_linha 
+        except ZeroDivisionError:
+            print(f"O valor da derivada da função no ponto {x} é 0!\n")
+            break
+
+        # Imprindo a iteração atual:
+        print(f"\nIteração {i}:")
+        print(f"\tx = {x}\n\tf(x) = {fx}\n\tValor absoluto do delta_x = {delta_x}")
+
+        # Verifica as condições de parada:
+        if abs(delta_x) < E1 or abs(fx) < E2:
+            break
+
+        x = x + delta_x # obtém o valor do próximo x
+        i += 1 # atualiza para a próxima iteração
 
 
 # Método da Secante:
