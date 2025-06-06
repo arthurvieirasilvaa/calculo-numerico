6a4fbb324631194644aa7d17e65133ed2a09e582
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2025-06-06 10:38:17 -0300

    Encontrei os coeficientes a do poloninômio interpolador na forma canônica

--- trabalho05/main.py
+++ trabalho05/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import linalg
 
 # Função utilizada para calcular a Tabela de Diferenças Divididas:
 def diferencas_divididas(x, y):
@@ -43,15 +44,79 @@ def interpolacao_lagrange(x, y, X_ESTIMADO):
     return p
 
 
+# Função utilizada para obter os coeficientes a do polinômio interpolador na forma canônica:
+def obter_coeficientes(x, y):
+    vandermonde = np.vander(x, increasing=True) # gerando a matriz de Vandermonde
+    
+    # Retorna o vetor de coeficintes a, solucionando o sisteman Vandermonde x a = y 
+    return linalg.solve(vandermonde, y)
+
+
 X_ESTIMADO = 5.2 # valor de x que deseja-se estimar
 
 # Valores de x e y para a alternativa a)
 xa = np.array([0, 2, 4, 6, 8, 10])
 ya = np.array([0.9, 2, 2.8, 3.1, 5.9, 6], dtype="double")
 
+# Valores de x e y para a alternativa b)
+xb = np.array([0, 2, 4, 6, 8])
+yb = np.array([1, 9.389, 58.598, 409.429, 2988.958], dtype="double")
+
+# Valores de x e y para a alternativa c)
+xc = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16])
+yc = np.array([1, 7, 21, 22, 34, 34.5, 35, 64.5, 65], dtype="double")
+
+
+# a)
+print("\nA)")
+
+# Interpolação de Lagrange:
+print("\nInterpolação de Lagrange:")
+p = interpolacao_lagrange(xa, ya, X_ESTIMADO)
+print(f"\tP({X_ESTIMADO}) = {p}")
+
+# Interpolação de Newton:
+print("\nInterpolação de Newton:")
 T = diferencas_divididas(xa, ya)
 p = interpolacao_newton(xa, X_ESTIMADO, T)
-print(f"f({X_ESTIMADO}) = {p}")
+print(f"\tP({X_ESTIMADO}) = {p}")
 
-p = interpolacao_lagrange(xa, ya, X_ESTIMADO)
-print(f"f({X_ESTIMADO}) = {p}")
+# Coeficientes a do polinômio interpolador na forma canônica:
+a = obter_coeficientes(xa, ya)
+print(f"\tCoeficientes a = [{a}]")
+
+# b)
+print("\nB)")
+
+# Interpolação de Lagrange:
+print("\nInterpolação de Lagrange:")
+p = interpolacao_lagrange(xb, yb, X_ESTIMADO)
+print(f"\tP({X_ESTIMADO}) = {p}")
+
+# Interpolação de Newton:
+print("\nInterpolação de Newton:")
+T = diferencas_divididas(xb, yb)
+p = interpolacao_newton(xb, X_ESTIMADO, T)
+print(f"\tP({X_ESTIMADO}) = {p}")
+
+# Coeficientes a do polinômio interpolador na forma canônica:
+a = obter_coeficientes(xb, yb)
+print(f"\tCoeficientes a = [{a}]")
+
+# c)
+print("\nC)")
+
+# Interpolação de Lagrange:
+print("\nInterpolação de Lagrange:")
+p = interpolacao_lagrange(xc, yc, X_ESTIMADO)
+print(f"\tP({X_ESTIMADO}) = {p}")
+
+# Interpolação de Newton:
+print("\nInterpolação de Newton:")
+T = diferencas_divididas(xc, yc)
+p = interpolacao_newton(xc, X_ESTIMADO, T)
+print(f"\tP({X_ESTIMADO}) = {p}")
+
+# Coeficientes a do polinômio interpolador na forma canônica:
+a = obter_coeficientes(xc, yc)
+print(f"\tCoeficientes a = [{a}]")
