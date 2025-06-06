8ebecee4510c70458e2e6ab4fc2884ee41dc3af9
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2025-06-06 10:00:33 -0300

    Implementei a Interpolação de Newton

--- trabalho05/main.py
+++ trabalho05/main.py
@@ -3,50 +3,44 @@ import numpy as np
 # Função utilizada para calcular a Tabela de Diferenças Divididas:
 def diferencas_divididas(x, y):
     n = len(x)
-    t = []
-    t.append(y)
+    T = np.zeros((n,n))
+    T [:, 0] = y[:] # diferenças dividas de ordem 0 é o próprio y
 
-    passo = 1 # deslocamento inicial no vetor x
-    for i in range(n-1):
-        ordem = [] # armazena a ordem das diferenças
-        for j in range(len(t[i])-1):
-            diferenca = (t[i][j+1] - t[i][j]) / (x[j+passo] - x[j])
-            ordem.append(diferenca)
-        t.append(ordem)
-        passo += 1 # atualiza o deslocamento no vetor x
+    for k in range(1, n):
+        for i in range(n-k):
+            T[i][k] = (T[i+1, k-1] - T[i, k-1]) / (x[i+k] - x[i])
     
-    return t
+    return T 
 
 
-# Função utilizada para estimar valor y de um dado ponto x utilizando a fórmula de Newton:
-def interpolacao_newton(x, t, x_val):
-    p = 0 # valor inicial da aproximação de f(x)
-    grau = 0 # grau atual do polinômio
+# Função utilizada para estimar valor y de um dado ponto x utilizando a Interpolação de Newton:
+def interpolacao_newton(x, X_ESTIMADO, T):
+    n = len(x)
 
-    print(f"Ponto estimado: {x_val}")
-
-    for i in range(len(t)):
-        for j in range(grau): 
-            t[i][0] *= (x_val - x[j]) # calcula o produto presente na fórumala de Newton
-        grau += 1 # atualiza o grau do polinômio
-        p += t[i][0] # atualiza a aproximação de f(x)
-        # print(f"Função P_{i}(x) = {p} incremento = {t[i][0]}") arrumar
+    p = 0
+    for i in range(n):
+        resp = 1
+        for j in range(i):
+            resp *= (X_ESTIMADO - x[j]) # calcula o produto presente na fórumala de Newton
+        p += T[0][i] * resp # atualiza a aproximação de f(x)
 
     return p
 
+
+# Função utilizada para estimar valor y de um dado ponto x utilizando a Interpolação de Lagrange:
 def interpolacao_lagrange(x, y, X_ESTIMADO):
     n = len(x)
     
-    y_encontrado = 0
+    p = 0
     for i in range(n):
         l = 1
         for j in range(n):
             if i != j:
                 l *= (X_ESTIMADO-x[j]) / (x[i]-x[j])
         
-        y_encontrado += y[i] * l
+        p += y[i] * l
 
-    return y_encontrado
+    return p
 
 
 X_ESTIMADO = 5.2 # valor de x que deseja-se estimar
@@ -55,8 +49,9 @@ X_ESTIMADO = 5.2 # valor de x que deseja-se estimar
 xa = np.array([0, 2, 4, 6, 8, 10])
 ya = np.array([0.9, 2, 2.8, 3.1, 5.9, 6], dtype="double")
 
-# t = diferencas_divididas(x, y)
-# p = interpolacao_newton(x, t, x_val)
+T = diferencas_divididas(xa, ya)
+p = interpolacao_newton(xa, X_ESTIMADO, T)
+print(f"f({X_ESTIMADO}) = {p}")
 
-y_encontrado = interpolacao_lagrange(xa, ya, X_ESTIMADO)
-print(f"f({X_ESTIMADO}) = {y_encontrado}")
+p = interpolacao_lagrange(xa, ya, X_ESTIMADO)
+print(f"f({X_ESTIMADO}) = {p}")
