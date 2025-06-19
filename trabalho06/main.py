602fcce2a0d6f8367ee787e257aa7b8da14e950b
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2025-06-19 19:06:57 -0300

    Implementei o 1° método de Simpson

--- trabalho06/main.py
+++ trabalho06/main.py
@@ -48,6 +48,35 @@ def metodo_trapezio(f, a, b, m):
     return (h/2) * res
 
 
+# Função utilizada para aproximar a integral da função f(x) com o método de Simpson (1/3):
+def metodo_simpson(f, a, b, m):
+    if m < 3:
+        print("Informe pelo menos 3 pontos!\n")
+        return
+    
+    n = m-1
+    print(f"{n} subintervalos")
+
+    if n % 2 != 0:
+        print("O número de subintervalos deve ser par!\n")
+
+    h = (b-a) / n
+
+    print("\nMétodo de Simpson (1/3):")
+    x = np.linspace(a, b, m)
+    print(f"Pontos utilizados x = {x}")
+
+    res = f(x[0])
+    for i in range(1, n, 2):
+        res += 4 * f(x[i]) # índices ímpares
+
+    for i in range(2, n-1, 2):
+        res += 2 * f(x[i]) # índices pares com exceção de f[x0] e f[xn]
+    
+    res += f(x[n])
+    return (h/3) * res
+
+
 # Inicializando as variáveis:
 
 # 1)
@@ -56,32 +85,53 @@ def metodo_trapezio(f, a, b, m):
 a = 0
 b = 2
 
+print(f"Intervalo [a, b] = [{a}, {b}]")
+
 # Método do trapézio:
 
-# Número de pontos para o método do trapézio
-mt1 = 2
-mt2 = 5
-mt3 = 13
-
-print(f"Intervalo [a, b] = [{a}, {b}]")
+# Número de pontos:
+m1 = 2
+m2 = 5
+m3 = 13
 
 analitico = integral_analitica1(a, b)
 print(f"Integral analítica: {analitico}")
 
-res_trapezio = metodo_trapezio(f1, a, b, mt1)
+res_trapezio = metodo_trapezio(f1, a, b, m1)
 print(f"Resultado da integral com o método do trapézio: {res_trapezio}")
 erro_trapezio = abs(analitico - res_trapezio)
 print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
 
-res_trapezio = metodo_trapezio(f1, a, b, mt2)
+res_trapezio = metodo_trapezio(f1, a, b, m2)
 print(f"Resultado da integral com o método do trapézio: {res_trapezio}")
 erro_trapezio = abs(analitico - res_trapezio)
 print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
 
-res_trapezio = metodo_trapezio(f1, a, b, mt3)
+res_trapezio = metodo_trapezio(f1, a, b, m3)
 print(f"Resultado da integral com o método do trapézio: {res_trapezio}")
 erro_trapezio = abs(analitico - res_trapezio)
 print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
+
+# 1° Método de Simpson:
+
+# Número de pontos:
+m1 = 3
+
+res_simpson = metodo_simpson(f1, a, b, m1)
+print(f"Resultado da integral com o 1° método de Simpson: {res_simpson}")
+erro_simpson = abs(analitico - res_simpson)
+print(f"Erro de truncamento para o 1° método de Simpson: {erro_simpson}")
+
+res_simpson = metodo_simpson(f1, a, b, m2)
+print(f"Resultado da integral com o 1° método de Simpson: {res_simpson}")
+erro_simpson = abs(analitico - res_simpson)
+print(f"Erro de truncamento para o 1° método de Simpson: {erro_simpson}")
+
+res_simpson = metodo_simpson(f1, a, b, m2)
+print(f"Resultado da integral com o 1° método de Simpson: {res_simpson}")
+erro_simpson = abs(analitico - res_simpson)
+print(f"Erro de truncamento para o 1° método de Simpson: {erro_simpson}")
+
 
 # 2)
 
@@ -89,41 +139,68 @@ print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
 a = 0
 b = 20
 
+print(f"Intervalo [a, b] = [{a}, {b}]")
+
 # Método do trapézio:
 
-# Número de pontos para o método do trapézio
-mt1 = 2
-mt2 = 5
-mt3 = 99
-mt4 = 599
-mt5 = 10001
-
-print(f"Intervalo [a, b] = [{a}, {b}]")
+# Número de pontos:
+m1 = 2
+m2 = 5
+m3 = 99
+m4 = 599
+m5 = 10001
 
 analitico = integral_analitica1(a, b)
 print(f"Integral analítica: {analitico}")
 
-res_trapezio = metodo_trapezio(f2, a, b, mt1)
+res_trapezio = metodo_trapezio(f2, a, b, m1)
 print(f"Resultado da integral com o método do trapézio: {res_trapezio}")
 erro_trapezio = abs(analitico - res_trapezio)
 print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
 
-res_trapezio = metodo_trapezio(f2, a, b, mt2)
+res_trapezio = metodo_trapezio(f2, a, b, m2)
 print(f"Resultado da integral com o método do trapézio: {res_trapezio}")
 erro_trapezio = abs(analitico - res_trapezio)
 print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
 
-res_trapezio = metodo_trapezio(f2, a, b, mt3)
+res_trapezio = metodo_trapezio(f2, a, b, m3)
 print(f"Resultado da integral com o método do trapézio: {res_trapezio}")
 erro_trapezio = abs(analitico - res_trapezio)
 print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
 
-res_trapezio = metodo_trapezio(f2, a, b, mt4)
+res_trapezio = metodo_trapezio(f2, a, b, m4)
 print(f"Resultado da integral com o método do trapézio: {res_trapezio}")
 erro_trapezio = abs(analitico - res_trapezio)
 print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
 
-res_trapezio = metodo_trapezio(f2, a, b, mt5)
+res_trapezio = metodo_trapezio(f2, a, b, m5)
 print(f"Resultado da integral com o método do trapézio: {res_trapezio}")
 erro_trapezio = abs(analitico - res_trapezio)
 print(f"Erro de truncamento para o método do trapézio: {erro_trapezio}")
+
+# 1° Método de Simpson:
+
+res_simpson = metodo_simpson(f2, a, b, m1)
+print(f"Resultado da integral com o 1° método de Simpson: {res_simpson}")
+erro_simpson = abs(analitico - res_simpson)
+print(f"Erro de truncamento para o 1° método de Simpson: {erro_simpson}")
+
+res_simpson = metodo_simpson(f2, a, b, m2)
+print(f"Resultado da integral com o 1° método de Simpson: {res_simpson}")
+erro_simpson = abs(analitico - res_simpson)
+print(f"Erro de truncamento para o 1° método de Simpson: {erro_simpson}")
+
+res_simpson = metodo_simpson(f2, a, b, m3)
+print(f"Resultado da integral com o 1° método de Simpson: {res_simpson}")
+erro_simpson = abs(analitico - res_simpson)
+print(f"Erro de truncamento para o 1° método de Simpson: {erro_simpson}")
+
+res_simpson = metodo_simpson(f2, a, b, m4)
+print(f"Resultado da integral com o 1° método de Simpson: {res_simpson}")
+erro_simpson = abs(analitico - res_simpson)
+print(f"Erro de truncamento para o 1° método de Simpson: {erro_simpson}")
+
+res_simpson = metodo_simpson(f2, a, b, m5)
+print(f"Resultado da integral com o 1° método de Simpson: {res_simpson}")
+erro_simpson = abs(analitico - res_simpson)
+print(f"Erro de truncamento para o 1° método de Simpson: {erro_simpson}")
