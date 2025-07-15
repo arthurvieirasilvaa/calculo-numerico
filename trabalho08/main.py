a1a1fab22c9fe0974cd81b52c11c436c10c847f9
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2025-07-15 10:02:30 -0300

    Imprimi a saída

--- trabalho08/main.py
+++ trabalho08/main.py
@@ -4,32 +4,59 @@ def f(t, y):
     return y + np.exp(2*t) + np.sin(t) + np.cos(t)
 
 
-def euler_simpes(f, t0, x0, tf, n):
+def euler_simpes(f, t0, y0, tf, n):
     h = (tf - t0) / n
-    x = np.zeros(n)
+    y = np.zeros(n)
     t = np.zeros(n)
 
-    x[0] = x0
+    y[0] = y0
     t[0] = t0
     for i in range(1, n):
-        x[i] = x[i-1] + h * f(t[i-1], x[i-1])
+        y[i] = y[i-1] + h * f(t[i-1], y[i-1])
         t[i] = t[i-1] + h   
     
-    return x, t
+    return y, t
 
 
-def euler_melhorado(f, t0, x0, tf, n):
+def euler_melhorado(f, t0, y0, tf, n):
     h = (tf - t0) / n
-    x = np.zeros(n)
+    y = np.zeros(n)
     t = np.zeros(n)
 
-    x[0] = x0
+    y[0] = y0
     t[0] = t0
     for i in range(1, n):
-        x_previsao = x[i-1] + h * f(t[i-1], x[i-1])
-        x[i] = x[i-1] + (h/2) * (f(t[i-1], x[i-1]) + f(t[i-1]+h, x_previsao))
+        y_previsao = y[i-1] + h * f(t[i-1], y[i-1])
+        y[i] = y[i-1] + (h/2) * (f(t[i-1], y[i-1]) + f(t[i-1]+h, y_previsao))
         t[i] = t[i-1] + h
     
-    return x, t
+    return y, t
 
 
+# Valores utilizados nos Métodos de Euler:
+t0 = 0
+y0 = 0
+tf = 2
+
+# a) Método de Euler Simples com 16 passos (subintervalos):
+print("\n========= Método de Euler Simples =========\n")
+ya, ta = euler_simpes(f, t0, y0, tf, 16)
+print(f"t = {ta}\n y = {ya}")
+
+# b) Método de Euler Melhorado com 8 passos:
+print("\n========= Método de Euler Melhorado =========\n")
+yb, tb = euler_melhorado(f, t0, y0, tf, 8)
+print(f"t = {tb}\n y = {yb}")
+
+# c) Método de Euler Simples com 64 passos (subintervalos):
+print("\n========= Método de Euler Simples =========\n")
+yc, tc = euler_simpes(f, t0, y0, tf, 64)
+print(f"t = {tc}\n y = {yc}")
+
+# d) Método de Euler Melhorado com 32 passos:
+print("\n========= Método de Euler Melhorado =========\n")
+yd, td = euler_melhorado(f, t0, y0, tf, 32)
+print(f"t = {td}\n y = {yd}")
+
+valor_exato = np.exp(2*tf) - np.cos(tf)
+print(f"\nValor exato de x(4) = {valor_exato}")
